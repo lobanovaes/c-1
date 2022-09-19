f3709fff0af9ff8b67d6ab3eaca0e694b51a3364
evgeniay <es.trepova@mail.ru>
2022-09-20 00:08:34 +0700

    Вывести промежуток цифр

---
+++ задача5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int number = Math.Abs(N);
+
+for (int i = number * (-1); i <= number; i++) 
+{
+    Console.Write($"{i} ");
+}
+Console.WriteLine("");

1579cf9a44117fb608959474df0e99e0fbca1437
evgeniay <es.trepova@mail.ru>
2022-09-20 01:43:28 +0700

    Четное/нечетное

---
+++ задача6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите число ");
+int N = int.Parse(Console.ReadLine());
+
+if(N % 2 == 0)
+{
+    Console.WriteLine($"{N} является четным" );
+}
+else
+{
+    Console.WriteLine($"{N} является не четным" );
+}

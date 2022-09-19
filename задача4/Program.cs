f677fef0b902c14ea89b6cd7cc26ec846d279405
evgeniay <es.trepova@mail.ru>
2022-09-20 00:48:02 +0700

    Испраления, промежуток цифр

--- задача4/Program.cs
+++ задача4/Program.cs
@@ -1,11 +1,12 @@
 ﻿Console.Clear();
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-if(number<99)
+
+Console.Write("Введите трехзначное число: ");
+int N = Math.Abs(int.Parse(Console.ReadLine()));
+
+if(N < 99 || N > 999 )
 {
-    Console.WriteLine("Введен неправильный номер" );
+    Console.WriteLine("Вы ввели не трехзначное число" );
+    return;
 }
-else 
-{
-    Console.WriteLine("Последняя цифра номера =" );
-}
+int division = N % 10;
+Console.WriteLine($"{division} Последняя цифра числа {N}" );

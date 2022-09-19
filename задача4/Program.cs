91b5e1f8118e843f69420109a4a2e4fcb540a759
evgeniay <es.trepova@mail.ru>
2022-09-19 23:50:38 +0700

    Вывести последнюю цифру

---
+++ задача4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number<99)
+{
+    Console.WriteLine("Введен неправильный номер" );
+}
+else 
+{
+    Console.WriteLine("Последняя цифра номера =" );
+}
